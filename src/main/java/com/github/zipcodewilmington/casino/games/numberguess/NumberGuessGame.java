1e09e069df41c2b867a4b3367ed6881623c74f6d
mahala <mahala.duffy@gmail.com>
2026-03-26 16:56:03 -0400

    Edits To Game and Player

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -1,12 +1,83 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
 
+import java.util.Random;
+import java.util.Scanner;
+
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 
-/**
- * Created by leon on 7/21/2020.
- */
+
 public class NumberGuessGame implements GameInterface {
+
+    private final NumberGuessPlayer player;
+    private final Random random;
+    private final Scanner scanner;
+    
+    private int number;
+    private int guesses;
+    private int guess;
+    private boolean playAgain;
+    
+
+    public NumberGuessGame(NumberGuessPlayer player, Random random, Scanner scanner) {
+        this.player = player;
+        this.random = random;
+        this.scanner = scanner;
+    }
+
+    public void play() {
+        System.out.println("Welcome to Number Guess Game!===");
+        
+        boolean playAgain = true;
+
+        while (playAgain) {
+            playRound();
+        } 
+
+        System.out.print("Do you want to play again? (yes/no): ");
+        String response = scanner.next();
+        playAgain = response.equalsIgnoreCase("yes");
+      
+        
+        System.out.println("Thanks for playing Number Guess Game!"); 
+        }
+    
+        private void playRound() {
+            number = random.nextInt(100) + 1; // Random number between 1 and 100
+            guesses = 0;    
+        
+        while (true) {
+            // Prompt the user for a guess
+            System.out.print("Enter a guess between 1 and 100: ");
+            int guess = scanner.nextInt();
+
+            // Count this guess
+            guesses ++;
+
+            // Check if the guess is correct
+            if (guess == number) {
+                System.out.println("CONGRATULATIONS YOU WIN! You guessed it in " + guesses + " guesses.");
+                break;
+            }
+            // Too low
+            else if (guess < number) {
+                System.out.println("Too Low!");
+            }
+            // Too high
+            else if (guess > number) {
+                System.out.println("Too High!");
+            }
+
+            // Check if the player has used all 10 guesses
+            if (guesses >= 10) {
+                System.out.println("You have run out of guesses! The number was " + number + ".");
+                break;
+            }
+        
+        }
+        scanner.close();
+    }
+
 
     @Override
     public void add(PlayerInterface player) {
@@ -20,6 +91,16 @@ public class NumberGuessGame implements GameInterface {
 
     @Override
     public void run() {
-        // TODO: implement the number guessing game loop
+    if (guess == number) {
+        System.out.println("CONGRATULATIONS YOU WIN! You guessed it in " + guesses + " guesses.");
+    }
+            // Too low
+    else if (guess < number) {
+        System.out.println("Too Low!");
+    }
+            // Too high
+    else if (guess > number) {
+        System.out.println("Too High!");
+    }   // TODO: implement the number guessing game loop
     }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayer.java
@@ -2,25 +2,44 @@ package com.github.zipcodewilmington.casino.games.numberguess;
 
 import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import java.util.Scanner;
 
-/**
- * Created by leon on 7/21/2020.
- */
 public class NumberGuessPlayer implements PlayerInterface {
 
-    private CasinoAccount casinoAccount;
+    private final CasinoAccount account; // Standardized name
+    private final Scanner scanner;
 
-    public NumberGuessPlayer(CasinoAccount casinoAccount) {
-        this.casinoAccount = casinoAccount;
+    public NumberGuessPlayer(CasinoAccount account) {
+        this.account = account;
+        this.scanner = new Scanner(System.in);
+    }
+
+    /**
+     * Logic for the player to input a guess.
+     */
+    public int makeGuess() {
+        System.out.print("Enter your guess (1–100): ");
+        // Check if input is an integer to avoid crashes
+        while (!scanner.hasNextInt()) {
+            System.out.print("That's not a number! Try again: ");
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
+    public CasinoAccount getAracadeAccount() {
+        // Changed from 'casinoAccount' to 'account' to match the field above
+        return account;
     }
 
     @Override
-    public CasinoAccount getArcadeAccount() {
-        return casinoAccount;
-    }
-
-    @Override
-    public <SomeReturnType> SomeReturnType play() {
-        return null; // TODO: implement guess logic
+    public void play() {
+        // Consolidated the two play() methods into one
+        System.out.println("Starting Number Guess Game...");
+        
+        // Example logic:
+        int userGuess = makeGuess();
+        System.out.println("You guessed: " + userGuess);
+        
     }
 }
